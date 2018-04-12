c41b162a729ebab2400d0c49176e42a6b17e7049
patryk603 <patryk603@wp.pl>
2018-04-12 14:06:16 +0200

    Add PRE2LOT and test prices

--- src/test/java/PRE2LOT/Booking.java
+++ src/test/java/PRE2LOT/Booking.java
@@ -3,11 +3,14 @@ package PRE2LOT;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import PRE2_pageObjects.HomePage;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Booking {
@@ -20,7 +23,7 @@ public class Booking {
     public void setUp() throws Exception {
         driver = new ChromeDriver();
         driver.get("https://prodcopy-pre2:MHXozAuh4eeCgK4x6ww6@pre2.book.lot.com/");
-        driver.manage().timeouts().implicitlyWait(2220, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         HomePage HomePage =PageFactory.initElements(driver, HomePage.class);
     }
 
@@ -47,12 +50,43 @@ public class Booking {
         HomePage.ListFrom.click();
         HomePage.To.sendKeys("JFK",Keys.ARROW_DOWN,Keys.ENTER);
 
-        String cena = driver.findElement(By.id("_availabilityportlet_WAR_lotairwaysportlet_precio-1-EUPINKTAN-3")).getAttribute("value");
-        System.out.println(cena);
+        //Lista cen
+        List<String> Ceny = new ArrayList<String>();
+        List<WebElement> Cena = driver.findElements(
+                By.name("precio"));
+        for (WebElement w : Cena) {
+            Ceny.add(w.getAttribute("value"));
+        }
+        System.out.println(Ceny);
+        Reporter.log(String.valueOf(Ceny));
+        //Lista cen
 
-        //HomePage.LotEconomySaver.getText();
-        //HomePage.LotEconomySaver.click();
     }
+
+    @Test(groups=("p1"))
+    public void Test_From_WAW_TO_ICN_prices() throws Exception {
+
+        HomePage.Departure.click();
+        HomePage.Today.click();
+        HomePage.Return.click();
+        HomePage.FirsOfNextMounth.click();
+        HomePage.From.sendKeys("Waw");
+        HomePage.ListFrom.click();
+        HomePage.To.sendKeys("ICN",Keys.ARROW_DOWN,Keys.ENTER);
+
+        //Lista cen
+        List<String> Ceny = new ArrayList<String>();
+        List<WebElement> Cena = driver.findElements(
+                By.name("precio"));
+        for (WebElement w : Cena) {
+            Ceny.add(w.getAttribute("value"));
+        }
+        System.out.println(Ceny);
+        Reporter.log(String.valueOf(Ceny));
+        //Lista cen
+    }
+
+
 
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
