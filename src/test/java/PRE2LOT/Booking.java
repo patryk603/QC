46dd1f25aeb65b6244629fb2c662cc985e2505cf
patryk603 <patryk603@wp.pl>
2018-04-13 16:51:56 +0200

    Add PRE2LOT and test prices

--- src/test/java/PRE2LOT/Booking.java
+++ src/test/java/PRE2LOT/Booking.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Reporter;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import PRE2_pageObjects.HomePage;
 
 import java.util.ArrayList;
@@ -23,25 +21,29 @@ public class Booking {
     public void setUp() throws Exception {
         driver = new ChromeDriver();
         driver.get("https://prodcopy-pre2:MHXozAuh4eeCgK4x6ww6@pre2.book.lot.com/");
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(220, TimeUnit.SECONDS);
         HomePage HomePage =PageFactory.initElements(driver, HomePage.class);
     }
 
-    @Test(groups=("p1"))
-    public void Test_From_WAW_TO_JFK() throws Exception {
+    @BeforeMethod(alwaysRun = true)
+    public void BeforeMethod() throws Exception {
+        driver.get("https://prodcopy-pre2:MHXozAuh4eeCgK4x6ww6@pre2.book.lot.com/");
 
-        HomePage.Departure.click();
-        HomePage.Today.click();
-        HomePage.Return.click();
-        HomePage.FirsOfNextMounth.click();
-        HomePage.From.sendKeys("Waw");
-        HomePage.ListFrom.click();
-        HomePage.To.sendKeys("JFK",Keys.ARROW_DOWN,Keys.ENTER);
     }
 
-    @Test(groups=("p1"))
+    @AfterMethod(alwaysRun = true)
+    public void browserSetUp() {
+        driver.manage().deleteAllCookies();
+    }
+    @AfterClass(alwaysRun = true)
+    public void shutDownSelenium() {
+        driver.close();
+    }
+
+    @Test//(groups=("p1"))
     public void Test_From_WAW_TO_JFK_prices() throws Exception {
 
+        driver.get("https://pre2.book.lot.com/");
         HomePage.Departure.click();
         HomePage.Today.click();
         HomePage.Return.click();
@@ -59,13 +61,15 @@ public class Booking {
         }
         System.out.println(Ceny);
         Reporter.log(String.valueOf(Ceny));
+        Reporter.getCurrentTestResult();
         //Lista cen
 
     }
 
-    @Test(groups=("p1"))
+    @Test//(groups=("p1"))
     public void Test_From_WAW_TO_ICN_prices() throws Exception {
 
+        driver.get("https://pre2.book.lot.com/");
         HomePage.Departure.click();
         HomePage.Today.click();
         HomePage.Return.click();
@@ -83,14 +87,60 @@ public class Booking {
         }
         System.out.println(Ceny);
         Reporter.log(String.valueOf(Ceny));
+        Reporter.getCurrentTestResult();
         //Lista cen
+
     }
 
+    @Test//(groups=("p1"))
+    public void Test_From_WAW_TO_LUX_prices() throws Exception {
 
+        driver.get("https://pre2.book.lot.com/");
+        HomePage.Departure.click();
+        HomePage.Today.click();
+        HomePage.Return.click();
+        HomePage.FirsOfNextMounth.click();
+        HomePage.From.sendKeys("Waw");
+        HomePage.ListFrom.click();
+        HomePage.To.sendKeys("LUX",Keys.ARROW_DOWN,Keys.ENTER);
 
-    @AfterClass(alwaysRun = true)
-    public void tearDown() throws Exception {
-       // driver.quit();
+        //Lista cen
+        List<String> Ceny = new ArrayList<String>();
+        List<WebElement> Cena = driver.findElements(
+                By.name("precio"));
+        for (WebElement w : Cena) {
+            Ceny.add(w.getAttribute("value"));
+        }
+        System.out.println(Ceny);
+        Reporter.log(String.valueOf(Ceny));
+        Reporter.getCurrentTestResult();
+        //Lista cen
+
+    }
+
+    @Test//(groups=("p1"))
+    public void Test_From_WAW_TO_BCN_prices() throws Exception {
+
+        driver.get("https://pre2.book.lot.com/");
+        HomePage.Departure.click();
+        HomePage.Today.click();
+        HomePage.Return.click();
+        HomePage.FirsOfNextMounth.click();
+        HomePage.From.sendKeys("Waw");
+        HomePage.ListFrom.click();
+        HomePage.To.sendKeys("BCN",Keys.ARROW_DOWN,Keys.ENTER);
+
+        //Lista cen
+        List<String> Ceny = new ArrayList<String>();
+        List<WebElement> Cena = driver.findElements(
+                By.name("precio"));
+        for (WebElement w : Cena) {
+            Ceny.add(w.getAttribute("value"));
+        }
+        System.out.println(Ceny);
+        Reporter.log(String.valueOf(Ceny));
+        Reporter.getCurrentTestResult();
+        //Lista cen
 
     }
 
